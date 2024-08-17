34baaa209a104686ef4a612625e61a2fc2ca3926
PerpetuumG <perpetuumg@gmail.com>
2024-08-17 16:33:53 +0300

    add zustand, scroll category

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Container, Filters, ProductsGroupList, Title, TopBar} from '@/components/shared';
+import { Container, Filters, ProductsGroupList, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
@@ -19,22 +19,39 @@ export default function Home() {
           {/* Список товаров */}
           <div className={'flex-1'}>
             <div className={'flex flex-col gap-16'}>
-              <ProductsGroupList title={'Пиццы'} items={[
-                  {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                  {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                  {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                  {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                  {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-              ]}  categoryId={1}/>
-
-
-                <ProductsGroupList title={'Завтрак'} items={[
-                    {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                    {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                    {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                    {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                    {id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{price: 550}]},
-                ]}  categoryId={2}/>
+              <ProductsGroupList
+                title={'Пиццы'}
+                items={[
+                  { id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 2, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 3, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 4, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 5, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                ]}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title={'Комбо'}
+                items={[
+                  { id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 2, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 3, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 4, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                  { id: 5, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                ]}
+                categoryId={2}
+              />
+                <ProductsGroupList
+                    title={'Закуски'}
+                    items={[
+                        { id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                        { id: 2, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                        { id: 3, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                        { id: 4, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                        { id: 5, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
+                    ]}
+                    categoryId={3}
+                />
               Список товаров
             </div>
           </div>
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,6 +1,10 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useEffect, useRef } from 'react';
+import { useIntersection } from 'react-use';
 import { cn } from '@/lib/utils';
 import { ProductCard, Title } from '@/components/shared';
+import { useCategoryStore } from '@/store/category';
 
 interface Props {
   title: string;
@@ -17,8 +21,23 @@ export const ProductsGroupList: FC<Props> = ({
   listClassName,
   categoryId,
 }) => {
+  // Начало
+  const setActiveCategoryId = useCategoryStore(state => state.setActiveId);
+  // В зависимости от нахождения категории будет в меню и категории одновременно отображаться всё
+  const intersectionRef = useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
+  // Конец
+
   return (
-    <div className={cn('', className)}>
+    <div className={cn('', className)} id={title} ref={intersectionRef}>
       <Title text={title} size={'lg'} className={'font-extrabold mb-5'} />
 
       <div className={cn('grid grid-cols-3 gap-[50px]', className)}>
