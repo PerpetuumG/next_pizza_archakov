0c54720811dff6c5f5356013d219a90cefb0b998
PerpetuumG <perpetuumg@gmail.com>
2024-08-25 14:35:05 +0300

    list of products

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import { Container, Filters, ProductsGroupList, Title, TopBar } from '@/components/shared';
+import { prisma } from '@/prisma/prisma-client';
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Container className={'mt-10'}>
         <Title text={'Все пиццы'} size={'lg'} className={'font-extrabold'} />
       </Container>
 
-      <TopBar />
+      <TopBar categories={categories.filter(category => category.products.length > 0)} />
 
       <Container className={'mt-10 pb-14'}>
         <div className={'flex gap-[80px]'}>
@@ -19,40 +31,17 @@ export default function Home() {
           {/* Список товаров */}
           <div className={'flex-1'}>
             <div className={'flex flex-col gap-16'}>
-              <ProductsGroupList
-                title={'Пиццы'}
-                items={[
-                  { id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 2, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 3, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 4, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 5, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                ]}
-                categoryId={1}
-              />
-              <ProductsGroupList
-                title={'Комбо'}
-                items={[
-                  { id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 2, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 3, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 4, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                  { id: 5, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                ]}
-                categoryId={2}
-              />
-                <ProductsGroupList
-                    title={'Закуски'}
-                    items={[
-                        { id: 1, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                        { id: 2, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                        { id: 3, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                        { id: 4, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                        { id: 5, name: 'Чизбургер-пицца', imageUrl: '', items: [{ price: 550 }] },
-                    ]}
-                    categoryId={3}
-                />
-              Список товаров
+              {categories.map(
+                category =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  ),
+              )}
             </div>
           </div>
         </div>
