d2d4a5f4f46a830fb1fc347d22f1f34e1490dca3
PerpetuumG <perpetuumg@gmail.com>
2024-08-25 15:32:27 +0300

    productImage, groupVariant

--- components/shared/top-bar.tsx
+++ components/shared/top-bar.tsx
@@ -4,7 +4,6 @@ import { Container } from '@/components/shared/container';
 import { Categories } from '@/components/shared/categories';
 import { SortPopup } from '@/components/shared/sort-popup';
 import { Category } from '@prisma/client';
-import { categories } from '@/prisma/constants';
 
 interface Props {
   categories: Category[];
