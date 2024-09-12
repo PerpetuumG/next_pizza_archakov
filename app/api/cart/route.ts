b004f04d57356d9978935407b141d1a5a0e8b913
PerpetuumG <perpetuumg@gmail.com>
2024-09-12 13:45:29 +0300

    function findOrCreateCart

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/prisma/prisma-client';
+import crypto from 'crypto';
 
 export async function GET(req: NextRequest) {
   try {
@@ -36,8 +37,22 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.json(userCart);
   } catch (e) {
-    console.error(e);
+    console.log('[CART_GET] Server error', e);
+    return NextResponse.json({ message: 'Не удалось получить корзину' }, { status: 500 });
   }
 }
 
+export async function POST(req: NextRequest) {
+  try {
+    let token = req.cookies.get('cartToken')?.value;
 
+    if (!token) {
+      token = crypto.randomUUID();
+    }
+
+
+  } catch (e) {
+    console.log('[CART_POST] Server error', e);
+    return NextResponse.json({ message: 'Не удалось создать корзину' }, { status: 500 });
+  }
+}
