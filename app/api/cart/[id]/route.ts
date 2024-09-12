e5356b4b0318a8465b21120c6fb4cd7551195250
PerpetuumG <perpetuumg@gmail.com>
2024-09-12 13:07:20 +0300

    DELETE cart

--- app/api/cart/[id]/route.ts
+++ app/api/cart/[id]/route.ts
@@ -39,3 +39,36 @@ export async function PATCH(req: NextRequest, { params }: { params: { id: string
     return NextResponse.json({ message: 'Не удалось обновить корзину' }, { status: 500 });
   }
 }
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const id = Number(params.id);
+    const token = req.cookies.get('cartToken')?.value;
+
+    if (!token) {
+      return NextResponse.json({ error: 'Cart token not found' });
+    }
+
+    const cartItem = await prisma.cartItem.findFirst({
+      where: {
+        id: Number(params.id),
+      },
+    });
+
+    if (!cartItem) {
+      return NextResponse.json({ error: 'Cart item not found' });
+    }
+
+    await prisma.cartItem.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+
+    const updateUserCart = await updateCartTotalAmount(token);
+    return NextResponse.json(updateUserCart);
+  } catch (e) {
+    console.error('[CART_DELETE] Server error', e);
+    return NextResponse.json({ message: 'Не удалось удалить корзину' }, { status: 500 });
+  }
+}
