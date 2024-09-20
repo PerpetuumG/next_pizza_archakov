e56850026bd4e616e351f407fdeeeffaa4cd6bdf
PerpetuumG <perpetuumg@gmail.com>
2024-09-20 15:52:40 +0300

    payOrderTemplate

---
+++ shared/components/email-templates/pay-order-template.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { cn } from '@/shared/lib/utils';
+
+interface Props {
+  className?: string;
+  orderId: number;
+  totalAmount: number;
+  paymentUrl: string;
+}
+
+export const PayOrderTemplate: FC<Props> = ({ className, orderId, totalAmount, paymentUrl }) => {
+  return (
+    <div className={cn('', className)}>
+      <h1>Заказ № {orderId}</h1>
+
+      <p>
+        Оплатите заказ на сумму {totalAmount}$. Перейдите <a href={paymentUrl}>по этой ссылке </a>{' '}
+        для оплаты заказа.
+      </p>
+    </div>
+  );
+};
