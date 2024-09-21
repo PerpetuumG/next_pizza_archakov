f064e957afb35f953f271d620fc0375de1dfa8c5
PerpetuumG <perpetuumg@gmail.com>
2024-09-21 14:30:22 +0300

    function createPayment

--- shared/lib/create-payment.ts
+++ shared/lib/create-payment.ts
@@ -1,19 +1,34 @@
 import axios from 'axios';
+import { PaymentData } from '@/@types/yookassa';
 
 export async function createPayment(details: any) {
-  const { data } = await axios.post('https://api.yookassa.ru/v3/payments', {
-    amount: {
-      value: details.amount,
-      currency: 'RUB',
+  const { data } = await axios.post<PaymentData>(
+    'https://api.yookassa.ru/v3/payments',
+    {
+      amount: {
+        value: details.amount,
+        currency: 'USD',
+      },
+      capture: true,
+      description: details.description,
+      metadata: {
+        order_id: details.order_id,
+      },
+      confirmation: {
+        type: 'redirect',
+        return_url: process.env.YOOKASSA_CALLBACK_URL,
+      },
     },
-    capture: true,
-    description: details.description,
-    metadata: {
-      order_id: details.order_id,
+    {
+      auth: {
+        username: process.env.YOOKASSA_API_KEY as string,
+        password: '',
+      },
+      headers: {
+        'Idempotence-Key': Math.random().toString(36).substring(7),
+      },
     },
-    confirmation: {
-      type: 'redirect',
-      return_url: 'http://localhost:3000/?paid',
-    },
-  });
+  );
+
+  return data;
 }
